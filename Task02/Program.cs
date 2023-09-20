b3900fe4f40d96ee5aad0e3d6ba1c9b121a61b9e
Gleb <eg1994@rambler.ru>
2023-09-20 12:38:11 +0300

    Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -8,6 +8,8 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите 2 число ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
-{
-    Console.WriteLine($"number1 > number2 ");
-}
+Console.WriteLine($"max = {number1} ");
+else if (number1 < number2)
+Console.WriteLine($" max = {number2} ");
+else if (number1 == number2)
+Console.WriteLine($"{number1} = {number2} ");
