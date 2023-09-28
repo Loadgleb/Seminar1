0f6edab02d352837c2d7652027cc948354262255
Gleb <eg1994@rambler.ru>
2023-09-28 22:58:02 +0300

    Lesson4

---
+++ Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Factorial(int num)
+{
+    int res = 1; 
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+
+            res *= i;
+        }
+        
+    }
+    return res;
+}
+
+Console.WriteLine("Введите целое положительное чило");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение чисел {number} -> {factorial} ");
---
+++ Task30/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+
+
+
+int[] CreatArray(int size)
+{
+    int[] array = new int[8];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+
+    }
+}
+
+int[] arr = CreatArray(8);
+PrintArray (arr);
