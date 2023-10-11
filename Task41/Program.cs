cae7243e4b0986736d1fa30f82cece617297051d
Gleb <eg1994@rambler.ru>
2023-10-11 21:42:29 +0300

    HomeWork5/1

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -11,3 +11,36 @@ int Promt(string message)
     int result = Convert.ToInt32(value);
     return result;
 }
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Promt($"Введите {i + 1}-й элемент");
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Writeline($"a[{i}] = {array[i]}");
+    }
+}
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int lenght = Prompt("Введите количество элементов >");
+int[] array;
+array = InputArray(lenght);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
